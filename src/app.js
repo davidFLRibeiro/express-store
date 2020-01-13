4b75563e51e35e0093d0843c37bd46a56394c3b1
DR <davidflribeiro87@gmail.com>
2020-01-13 17:23:06 -0100

    first commit

---
+++ src/app.js
@@ -0,0 +1,62 @@
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
+const { NODE_ENV } = require('./config');
+
+const app = express();
+
+const morganOption = NODE_ENV === 'production' ? 'tiny' : 'common';
+
+app.use(morgan(morganOption));
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('A GET Request');
+});
+
+app.post('/', (req, res) => {
+  console.log(req.body);
+  res.send('POST request received');
+  const { username, password, favoriteClub, newsLetter = false } = req.body;
+});
+
+if (!username) {
+  return res.status(400).send('Username Required');
+}
+
+if (!password) {
+  return res.status(400).send('password required');
+}
+
+if (!favoriteClub) {
+  return res.status(400).send('favorite Club required');
+}
+
+if (username.length < 6 || username.length > 20) {
+  return status(400).send('Username must be between 6 and 20 characters');
+}
+
+if (password.length < 8 || password.length > 36) {
+  return res.status(400).send('Password must be between 8 and 36 characters');
+}
+
+if (!password.match(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/)) {
+  return res.status(400).send('Password must be contain at least one digit');
+}
+
+app.use(function errorHandler(error, req, res, next) {
+  let response;
+  if (NODE_ENV === 'production') {
+    response = { error: { message: 'server error' } };
+  } else {
+    console.error(error);
+    response = { message: error.message, error };
+  }
+  res.status(500).json(response);
+});
+
+module.exports = app;
